0bd728152f74ecf9d35c3b42a2074ff79a536a4b
Alex Martin <martin.alex.t+github@gmail.com>
2015-02-01 16:16:12 -0500

    Polling

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 4567
-
-
+var remoteUrl = "http://tiny-pizza-server.herokuapp.com/collections/greenville-chats/"
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
@@ -19,9 +18,18 @@ var wss = new wsServer({server: server});
 console.log("Created WS Server");
 
 wss .on('connection', function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() { })
-  }, 1000); 
+  // var id = setInterval(function() {
+  //   ws.send(JSON.stringify(new Date()), function() { })
+  // }, 1000); 
+
+  var msgs = setInterval(function() {
+    http.get(remoteUrl, function(res) {
+    var body = '';
+    res
+      .on('data', function(chunk) { body += chunk; })
+      .on('end', function() { ws.send(JSON.parse(body)[0], function() {} });  
+    });
+  }, 1000);
   
   console.log("connection opened!");
   
